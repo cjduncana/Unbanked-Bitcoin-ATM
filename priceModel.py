3547f265245e4b117020e32821b1d07248f56622
Christopher Duncan <cj.duncan.a@gmail.com>
2015-11-15 15:37:47 -0500

    Create price model between bitcoin and physical currency

---
+++ priceModel.py
@@ -0,0 +1,22 @@
+
+from decimal import Decimal
+
+class PriceModel:
+
+	def __init__(self, totalAmountBills,
+                 currentAmountBills, currentAmountBitcoin):
+        self.totalAmountBills = totalAmountBills
+        self.currentAmountBills = currentAmountBills
+        self.currentAmountBitcoin = currentAmountBitcoin
+        self.eccentricity = Decimal("1")
+
+    def calculate(amountBills):
+        amountBitcoin = self.eccentricity * ((((self.totalAmountBills -
+                        amountBills - self.currentAmountBills) *
+                        (self.currentAmountBills)) / ((amountBills +
+                        self.currentAmountBills) *
+                        (self.totalAmountBills -
+                        self.currentAmountBills))).ln() +
+                        self.currentAmountBitcoin * ((1 -
+                        amountBills.exp()) / (amountBills.exp())))
+        return amountBitcoin
