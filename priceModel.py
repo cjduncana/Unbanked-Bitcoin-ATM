cac61af625b6cb6b05ebd0e6c211bedde55b8373
Christopher Duncan <cj.duncan.a@gmail.com>
2015-11-17 15:58:38 -0500

    Wrap long code lines the correct way

--- priceModel.py
+++ priceModel.py
@@ -3,7 +3,7 @@ import decimal
 
 class PriceModel:
 
-	def __init__(self, totalAmountBills,
+    def __init__(self, totalAmountBills,
                  currentAmountBills, currentAmountBitcoin):
         self.totalAmountBills = totalAmountBills
         self.currentAmountBills = currentAmountBills
@@ -11,12 +11,11 @@ class PriceModel:
         self.eccentricity = decimal.Decimal("1")
 
     def calculate(amountBills):
-        amountBitcoin = self.eccentricity * ((((self.totalAmountBills -
-                        amountBills - self.currentAmountBills) *
-                        (self.currentAmountBills)) / ((amountBills +
-                        self.currentAmountBills) *
-                        (self.totalAmountBills -
-                        self.currentAmountBills))).ln() +
-                        self.currentAmountBitcoin * ((1 -
-                        amountBills.exp()) / (amountBills.exp())))
+        amountBitcoin = self.eccentricity * ((((self.totalAmountBills \
+        - amountBills - self.currentAmountBills) \
+        * (self.currentAmountBills)) / ((amountBills \
+        + self.currentAmountBills) * (self.totalAmountBills \
+        - self.currentAmountBills))).ln() + self.currentAmountBitcoin \
+        * ((1 - amountBills.exp()) / (amountBills.exp())))
+
         return amountBitcoin
